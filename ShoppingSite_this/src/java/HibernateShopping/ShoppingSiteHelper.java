fa8576db26aa571b33c5a326479dc3b888a9cfec
Tobias <tobias.tangfelt@skyride.se>
2012-01-04 13:49:54 +0100

    JSF skapad för att hämta hem album
    Jag har nu skapat en grund där man kan välja en musikkategori som du
    presenterar albumen därefter. Användaren kan även se en bild utav det
    specifika albumet. Jag skall nu fixa en kundkorg där användaren kan
    placera varor och lite annat. Men slänger upp en första version iaf.

---
+++ ShoppingSite_this/src/java/HibernateShopping/ShoppingSiteHelper.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HibernateShopping;
+
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author tobiastangfelt
+ */
+public class ShoppingSiteHelper {
+    Session session = null;
+
+    public ShoppingSiteHelper() {
+        this.session = HibernateUtil.getSessionFactory().getCurrentSession();
+    }
+    //Hämtar album beroende på inmatad kategori
+    public List getMusicRecordings(String rec){
+    List<MusicCategories> musicList = null;
+    try {
+        org.hibernate.Transaction tx = session.beginTransaction();
+        Query q = session.createQuery ("from MusicRecordings WHERE category = '"+rec+"'");
+        musicList = (List<MusicCategories>) q.list();
+        
+
+    } catch (Exception e) {
+        session.close();
+    }
+
+    return musicList;
+}
+}
